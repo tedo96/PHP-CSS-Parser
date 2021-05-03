ca842a99926458af4450e4454b9b5e851cb2d1b3
Oliver Klee <github@oliverklee.de>
2021-05-03 20:55:57 +0200

    Autofix all autofixable PSR-12 code style issues
    These changes were all automatically done by PHP_CodeSniffer,
    and there are no manual changes.

--- lib/Sabberworm/CSS/Value/URL.php
+++ lib/Sabberworm/CSS/Value/URL.php
@@ -4,17 +4,20 @@ namespace Sabberworm\CSS\Value;
 
 use Sabberworm\CSS\Parsing\ParserState;
 
-class URL extends PrimitiveValue {
+class URL extends PrimitiveValue
+{
 
-	private $oURL;
+    private $oURL;
 
-	public function __construct(CSSString $oURL, $iLineNo = 0) {
-		parent::__construct($iLineNo);
-		$this->oURL = $oURL;
-	}
+    public function __construct(CSSString $oURL, $iLineNo = 0)
+    {
+        parent::__construct($iLineNo);
+        $this->oURL = $oURL;
+    }
 
-	public static function parse(ParserState $oParserState) {
-		$oParserState->setAnchor();
+    public static function parse(ParserState $oParserState)
+    {
+        $oParserState->setAnchor();
 		$sIdentifier = '';
 		for ($i = 0; $i < 3; $i++) {
 			$sChar = $oParserState->parseCharacter(true);
@@ -24,41 +27,44 @@ class URL extends PrimitiveValue {
 			$sIdentifier .= $sChar;
 		}
 		$bUseUrl = $oParserState->streql($sIdentifier, 'url');
-		if ($bUseUrl) {
-			$oParserState->consumeWhiteSpace();
-			$oParserState->consume('(');
-		} else {
+        if ($bUseUrl) {
+            $oParserState->consumeWhiteSpace();
+            $oParserState->consume('(');
+        } else {
 			$oParserState->backtrackToAnchor();
 		}
-		$oParserState->consumeWhiteSpace();
-		$oResult = new URL(CSSString::parse($oParserState), $oParserState->currentLine());
-		if ($bUseUrl) {
-			$oParserState->consumeWhiteSpace();
-			$oParserState->consume(')');
-		}
-		return $oResult;
-	}
+        $oParserState->consumeWhiteSpace();
+        $oResult = new URL(CSSString::parse($oParserState), $oParserState->currentLine());
+        if ($bUseUrl) {
+            $oParserState->consumeWhiteSpace();
+            $oParserState->consume(')');
+        }
+        return $oResult;
+    }
 
 
-	public function setURL(CSSString $oURL) {
-		$this->oURL = $oURL;
-	}
+    public function setURL(CSSString $oURL)
+    {
+        $this->oURL = $oURL;
+    }
 
-	public function getURL() {
-		return $this->oURL;
-	}
+    public function getURL()
+    {
+        return $this->oURL;
+    }
 
-	public function __toString() {
-		return $this->render(new \Sabberworm\CSS\OutputFormat());
-	}
+    public function __toString()
+    {
+        return $this->render(new \Sabberworm\CSS\OutputFormat());
+    }
 
-	/**
-	 * @param \Sabberworm\CSS\OutputFormat $oOutputFormat
-	 *
-	 * @return string
-	 */
-	public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat) {
-		return "url({$this->oURL->render($oOutputFormat)})";
-	}
-
+    /**
+     * @param \Sabberworm\CSS\OutputFormat $oOutputFormat
+     *
+     * @return string
+     */
+    public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat)
+    {
+        return "url({$this->oURL->render($oOutputFormat)})";
+    }
 }
